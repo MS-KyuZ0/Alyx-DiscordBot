d4ac1c6a2be4f6f8b3d189fa9deea5e39e9c43ef
MS-KyuZ0 <muhammadsadam729@gmail.com>
2026-01-08 20:07:30 +0800

    feat: add ping command with basic response

---
+++ src/Commands/Utils/ping.ts
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('ping').setDescription('Test bot respons.'),
+    async execute(interaction: { reply: (arg0: string) => any; user: { username: any; }; member: { joinedAt: any; }; }) {
+        await interaction.reply(
+            `Pong!`,
+        );
+    },
+};
