8547dcea866c41aa7c190d16367c1e0ec2914156
MS-KyuZ0 <muhammadsadam729@gmail.com>
2026-01-08 20:07:20 +0800

    refactor: reorganize imports and initialize Commands collection in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 require("dotenv").config();
-const {Client, Events} = require('discord.js');
-const DiscordBot = new Client({intents: 131071});
+const { Client, Events, Collection } = require('discord.js');
+const DiscordBot = new Client({ intents: 131071 });
 const eventHandler = require('./Handler/EventHandler');
+const commandHandler = require('./Handler/CommandHandler');
+
+DiscordBot.Commands = new Collection();
 
 eventHandler(DiscordBot);
+commandHandler(DiscordBot);
 
 DiscordBot.login(process.env.DISCORD_TOKEN);
