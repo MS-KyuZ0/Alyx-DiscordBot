ff58243272b56098c0ec17f1a18fb05b8c422da4
mskyuz0 <muhammadsadam729@gmail.com>
2026-01-12 02:54:01 +0800

    fix: format ping command builder for improved readability

--- src/Commands/Utils/ping.ts
+++ src/Commands/Utils/ping.ts
@@ -1,7 +1,9 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Test bot respons.'),
+    data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Test bot respons.'),
     async execute(interaction: { reply: (arg0: string) => any; user: { username: any; }; member: { joinedAt: any; }; }) {
         await interaction.reply(
             `Pong!`,
